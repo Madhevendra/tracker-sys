6c5076d6a217b531fb0e883c2d566e4c49a20f75
Madhevendra Singh <msingh2576@gmail.com>
2025-07-19 08:33:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ export default {
   darkMode: ['class'],
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
